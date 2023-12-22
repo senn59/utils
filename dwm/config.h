a0e02f1177d686399434c2c3b407585ad0dddfed
senn59 <74777070+senn59@users.noreply.github.com>
2023-12-22 11:12:12 +0100

    add fullscreen patch

--- dwm/config.h
+++ dwm/config.h
@@ -85,6 +85,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /* floating */
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /*  monocle */
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },/* multi monitor tings */
